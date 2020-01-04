7007e5b77720804c69eb82d2c672a8afacf8882a
Mike Meyer <mikemey01@gmail.com>
2020-01-03 17:04:25 -0700

    adding appsettings setup

--- ci.trading.app/Program.cs
+++ ci.trading.app/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using ci.trading.service.app;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
 
 namespace ci.trading.app
 {
     class Program
     {
+        static AppSettings appSettings = new AppSettings();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            var configuration = builder.Build();
+            ConfigurationBinder.Bind(configuration.GetSection("AppSettings"), appSettings);
+
+            Console.WriteLine(appSettings.ApiPaper);
         }
     }
 }
---
+++ ci.trading.app/app/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ci.trading.service.app
+{
+    public class AppSettings
+    {
+        public string ApiPaper { get; set; }
+        public string ApiLive { get; set; }
+    }
+}
