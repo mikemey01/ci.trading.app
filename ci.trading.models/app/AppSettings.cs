379a5aebd3724d7a931c8ed13a21c23a34a63c51
Mike Meyer <mikemey01@gmail.com>
2020-01-06 22:21:36 -0700

    removing appsettings

--- ci.trading.models/app/AppSettings.cs
+++ ci.trading.models/app/AppSettings.cs
@@ -8,8 +8,11 @@ namespace ci.trading.models.app
     {
         public string ApiPaperEndpoint { get; set; }
         public string ApiPaperKey { get; set; }
+        public string ApiPaperSecretKey { get; set; }
+
         public string ApiLiveEndpoint { get; set; }
         public string ApiLiveKey { get; set; }
+        public string ApiLiveSecretKey { get; set; }
 
         public AppSettings()
         {
