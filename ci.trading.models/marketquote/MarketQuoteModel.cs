7a62caf6f1b9d1ba0e10df3e52cdbb4fd8a5e89e
Mike Meyer <mikemey01@gmail.com>
2020-01-17 15:14:27 -0700

    adding rest of quote model props

--- ci.trading.models/marketquote/MarketQuoteModel.cs
+++ ci.trading.models/marketquote/MarketQuoteModel.cs
@@ -18,7 +18,34 @@ namespace ci.trading.models.marketquote
         public decimal AskSize { get; set; } // in hundreds
         public decimal Bid { get; set; }
         public decimal BidSize { get; set; } // in hundreds
-        public int MyProperty { get; set; }
+        public decimal BetaVolatility { get; set; }
+        public decimal PreviousCandleClose { get; set; }
+        public decimal Dividend { get; set; }
+        public DateTime? DividendExDate { get; set; }
+        public decimal EarningsPerShare { get; set; }
+        public decimal High { get; set; } // day high price
+        public decimal LastTradeVolume { get; set; }
+        public decimal LastTradePrice { get; set; }
+        public decimal Low { get; set; } //day low price
+        public string Name { get; set; }
+        public decimal Open { get; set; } // day open price
+        public decimal PriorDayClose { get; set; }
+        public decimal PriceEarnings { get; set; }
+        public decimal PriorDayHigh { get; set; }
+        public decimal PriorDayLow { get; set; }
+        public decimal PriorDayOpen { get; set; }
+        public decimal PriorDayVolume { get; set; }
+        public decimal SharesOutstanding { get; set; }
+        public decimal TradesSinceMarketOpen { get; set; }
+        public string TrendTenTicks { get; set; }
+        public decimal Volume { get; set; }
+        public decimal VolatilityOneYear { get; set; }
+        public decimal VolumeWeightedAveragePrice { get; set; }
+        public decimal High52Week { get; set; }
+        public DateTime? High52WeekDate { get; set; }
+        public decimal Low52Week { get; set; }
+        public DateTime? Low52WeekDate { get; set; }
+        public decimal DividendYieldAsPercent { get; set; }
 
     }
 }
